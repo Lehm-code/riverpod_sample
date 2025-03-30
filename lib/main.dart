1f5b3836e5af93d8e5e8ffc2a2a73d222d5af048
Lehm-win <kira.free.masato@gmail.com>
2025-03-30 18:06:02 +0900

    ズレて2倍になる状況

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  const app = MaterialApp(home: MyApp());
-
-  const scope = ProviderScope(child: app);
-
-  runApp(scope);
+  runApp(
+    // 状態管理する範囲を指定
+    const ProviderScope(
+      child: MyApp()
+    )
+  );
 }
 
-// カウンターの状態プロバイダ
+// カウンターの状態プロバイダを生成と初期値の代入
 final counterProvider = StateProvider<int>((ref) => 0);
+final doubleProvider = StateProvider<int>((ref) => 0);
+final doubleProvider2 = StateProvider<int>((ref) => 0);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Riverpod Demo Home Page'),
+      home: const MyHomePage(title: 'Riverpodデモ'),
     );
   }
 }
@@ -37,6 +40,8 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // プロバイダから状態を取得
     final counter = ref.watch(counterProvider);
+    final doubleCounter = ref.watch(doubleProvider);
+    final doubleCounter2 = ref.watch(doubleProvider2);
 
     return Scaffold(
       appBar: AppBar(
@@ -47,21 +52,34 @@ class MyHomePage extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('ボタンを押すと増えるよ'),
             Text(
               '$counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text('上の数字の2倍だよ'),
+            Text(
+              '$doubleCounter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              '$doubleCounter2',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(counterProvider.notifier).state++,
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          // 1つ前の数字を使ってしまう場合
+          ref.read(doubleProvider.notifier).state = counter * 2;
+          //ref.read(doubleProvider.notifier).state = counterProvider.state * 2;
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
